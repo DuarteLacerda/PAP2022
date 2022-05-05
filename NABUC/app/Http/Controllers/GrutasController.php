8f6bcacb77c69e7cd0a5d62f3d3ce6841920c66e
Duarte Lacerda <duartelacerda4@gmail.com>
2022-05-05 17:11:32 +0100

    05/05/2022
    Atualização na autenticação

---
+++ NABUC/app/Http/Controllers/GrutasController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Grutas;
+use Illuminate\Http\Request;
+
+class GrutasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Grutas  $grutas
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Grutas $grutas)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Grutas  $grutas
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Grutas $grutas)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Grutas  $grutas
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Grutas $grutas)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Grutas  $grutas
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Grutas $grutas)
+    {
+        //
+    }
+}
